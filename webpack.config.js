19e94712e179e794641bc95c33d108405b2ea827
TIMBIOZ-PC\Timbioz <volkovtim@gmail.com>
2018-02-15 22:31:24 +0300

    Add dotenv package. Add .env file for own environment variables. Configure webpack.config.js for using js by babel-loader and dotenv variables injection. Add APP_ENV for replacement NODE_ENV using

--- webpack.config.js
+++ webpack.config.js
@@ -1,55 +1,48 @@
+require('dotenv').config();
+
 var path = require('path');
-
-
-const NODE_ENV = process.env.NODE_ENV || 'development';
 const webpack = require('webpack');
 
-const output_path = NODE_ENV === 'production' ?
-    (__dirname + '/public/js'):
-    (__dirname + '/resources/assets/js');
+const isDev = process.env.APP_ENV === 'development';
 
-    console.log(process.env.NODE_ENV);
-    console.log(NODE_ENV);
-    console.log(__dirname);
+// TODO: add color npm package
+
+console.log(process.env.APP_ENV);
+console.log(isDev);
+
+const output_path = isDev ? path.resolve(__dirname, "/dist/js") : path.resolve(__dirname, "/public/js");
 
 module.exports = {
-    context: path.resolve(__dirname),
+  context: path.resolve(__dirname),
 
-    entry: {
-        main: './src/js/index'
+  entry: {
+    main: "./src/js/index"
+  },
+
+  output: {
+    path: output_path,
+    filename: "[name].js"
+  },
+
+  devtool: isDev ? "cheap-inline-module-source-map" : "source-map",
+
+  resolve: {
+    alias: {
+      jquery: path.resolve(__dirname, "./node_modules/jquery/dist/jquery.js"),
+      bootstrap_css: path.resolve(__dirname, "./node_modules/bootstrap/dist/css/bootstrap.css"),
+      bootstrap_js: path.resolve(__dirname, "./node_modules/bootstrap/dist/js/bootstrap.js"),
+      popper_js: path.resolve(__dirname, "./node_modules/popper.js/dist/popper.js")
     },
+    extensions: [".js", ".jsx", ".json", ".css", ".scss"]
+  },
 
-    output: {
-        path: output_path,
-        filename: '[name].js'
-    },
-
-    watch: NODE_ENV === 'development',
-
-    watchOptions: {
-        aggregateTimeout: 100
-    },
-
-    devtool: NODE_ENV === 'development' ? 'cheap-inline-module-source-map' : false,
-
-    resolve : {
-        alias: {
-          "jquery-ui": "jquery-ui-dist/jquery-ui.js"
-        },
-        extensions: ['.js', '.jsx', '.json', '*']
-    },
-
-    module: {
-        rules: [
-            {
-                test: [
-                    /\.jsx?$/
-                ],
-                exclude: /node_modules/,
-                use: [
-                    'babel-loader'
-                ]
-            }
-        ]
-    }
+  module: {
+    rules: [
+      {
+        test: [/\.jsx?$/],
+        exclude: /node_modules/,
+        use: ["babel-loader"]
+      }
+    ]
+  }
 };
