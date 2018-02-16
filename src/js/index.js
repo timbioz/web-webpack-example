bd18f8c385545573252942467727e9710a76a824
TIMBIOZ-PC\Timbioz <volkovtim@gmail.com>
2018-02-16 18:17:20 +0300

    Implemented transpilling es6 to es5 and css compiling with extract-text-webpack-plugin. Added Webpack Dev Server

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,15 @@
+import "../css/index.css"
 
-let id = 333;
+let id = 13356665111;
 
-const url = `http://localhost:3000/api/messages/${id}`;
+const url = `http://localhost:3000/api/messagesww/${id}`;
 
+function timbioz() {
+    alert(url);
+}
+
+document.getElementById ("timbioz").addEventListener ("click", timbioz, false);
+
+function myFunction() {
+  alert("Hello! I fwd an alert box!!");
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,21 @@
-require('dotenv').config();
-const path = require('path');
-const webpack = require('webpack');
+require("dotenv").config();
+const path = require("path");
+const webpack = require("webpack");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-const isDev = process.env.APP_ENV === 'development';
+const isDev = process.env.APP_ENV === "development";
+
+// TODO: resolve Live reload browser feature
 
 console.log(process.env.APP_ENV);
-console.log(isDev);
 
-const output_path = isDev ? path.resolve(__dirname, "/dist/js") : path.resolve(__dirname, "/public/js");
+const output_path = isDev
+  ? path.resolve(__dirname, "dist")
+  : path.resolve(__dirname, "public");
+
+const extractCSS = new ExtractTextPlugin({
+  filename: "css/[name].css"
+});
 
 module.exports = {
   context: path.resolve(__dirname),
@@ -18,28 +26,57 @@ module.exports = {
 
   output: {
     path: output_path,
-    filename: "[name].js"
+    publicPath: "dist",
+    filename: "js/[name].js"
   },
 
   devtool: isDev ? "cheap-inline-module-source-map" : "source-map",
 
-  resolve: {
-    alias: {
-      jquery: path.resolve(__dirname, "./node_modules/jquery/dist/jquery.js"),
-      bootstrap_css: path.resolve(__dirname, "./node_modules/bootstrap/dist/css/bootstrap.css"),
-      bootstrap_js: path.resolve(__dirname, "./node_modules/bootstrap/dist/js/bootstrap.js"),
-      popper_js: path.resolve(__dirname, "./node_modules/popper.js/dist/popper.js")
-    },
-    extensions: [".js", ".jsx", ".json", ".css", ".scss"]
+  devServer: {
+    contentBase: "./dist",
+    watchContentBase: true
   },
 
   module: {
     rules: [
       {
-        test: [/\.jsx?$/],
-        exclude: /node_modules/,
-        use: ["babel-loader"]
+        test: /\.jsx?$/,
+        exclude: /(node_modules|dist)/,
+        use: {
+          loader: "babel-loader",
+          query: {
+            presets: ["env"]
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: extractCSS.extract({
+          fallback: "style-loader",
+          use: "css-loader"
+         })
       }
-    ]
-  }
+    ],
+  },
+
+  resolve: {
+    alias: {
+      jquery: path.resolve(__dirname, "./node_modules/jquery/dist/jquery.js"),
+      bootstrap_css: path.resolve(
+        __dirname,
+        "./node_modules/bootstrap/dist/css/bootstrap.css"
+      ),
+      bootstrap_js: path.resolve(
+        __dirname,
+        "./node_modules/bootstrap/dist/js/bootstrap.js"
+      ),
+      popper_js: path.resolve(
+        __dirname,
+        "./node_modules/popper.js/dist/popper.js"
+      )
+    },
+    extensions: [".js", ".jsx", ".json", ".css", ".scss"]
+  },
+
+  plugins: [extractCSS]
 };
