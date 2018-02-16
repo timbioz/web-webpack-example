77570ae710edaf0bbed55d85d08db3fad201ebea
TIMBIOZ-PC\Timbioz <volkovtim@gmail.com>
2018-02-16 20:32:38 +0300

    Implemented Webpack-stream gulp task for automaticaly start webpack when source files chenged

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require("gulp");
 var browserSync = require("browser-sync").create();
-var reload      = browserSync.reload;
+var reload = browserSync.reload;
+var webpack = require("webpack-stream");
+
 // Static server
 gulp.task("browser-sync", function() {
     browserSync.init({
@@ -11,5 +13,17 @@ gulp.task("browser-sync", function() {
     });
 
     gulp.watch("dist/**/*").on("change", reload);
+});
+
+gulp.task("webpack", function() {
+    return gulp
+        .src("src/js/index.js")
+        .pipe(webpack(require("./webpack.config.js")))
+        .pipe(gulp.dest("dist/"));
+});
+
+
+gulp.task("webpack-watch", function() {
+    gulp.watch("src/**/*", ["webpack"]);
 });
 
