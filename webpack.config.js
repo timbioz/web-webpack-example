672f0e02ee3290f54dbc7fc05633f71c8122c6b5
TIMBIOZ-PC\Timbioz <volkovtim@gmail.com>
2018-02-16 21:18:14 +0300

    Added Webpack Clean plugin for cleanup build and dist folders before webpack processing

--- webpack.config.js
+++ webpack.config.js
@@ -1,81 +1,97 @@
 require("dotenv").config();
 const path = require("path");
 const webpack = require("webpack");
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+// region Options
 
 const isDev = process.env.APP_ENV === "development";
 
 console.log(process.env.APP_ENV);
 
 const output_path = isDev
-  ? path.resolve(__dirname, "build")
-  : path.resolve(__dirname, "dist");
+    ? path.resolve(__dirname, "build")
+    : path.resolve(__dirname, "dist");
 
 const extractCSS = new ExtractTextPlugin({
-  filename: "css/[name].css"
+    filename: "css/[name].css"
 });
 
+// the path(s) that should be cleaned
+let pathsToClean = ["build", "dist"];
+
+// the clean options to use
+let cleanOptions = {
+    verbose: true
+};
+
+// endregion
+
 module.exports = {
-  context: path.resolve(__dirname),
+    context: path.resolve(__dirname),
 
-  entry: {
-    main: "./src/js/index"
-  },
-
-  output: {
-    path: output_path,
-    //publicPath: "dist",
-    filename: "js/[name].js"
-  },
-
-  devtool: isDev ? "cheap-inline-module-source-map" : "source-map",
-
-  // TODO: delete devserver after BrowserSync will be fully tested
-  devServer: {
-    contentBase: "./dist",
-    watchContentBase: true
-  },
-
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        exclude: /(node_modules|dist|build)/,
-        use: {
-          loader: "babel-loader",
-          query: {
-            presets: ["env"]
-          }
-        }
-      },
-      {
-        test: /\.css$/,
-        use: extractCSS.extract({
-          fallback: "style-loader",
-          use: "css-loader"
-         })
-      }
-    ],
-  },
-
-  resolve: {
-    alias: {
-      jquery: path.resolve(__dirname, "./node_modules/jquery/dist/jquery.js"),
-      bootstrap_css: path.resolve(
-        __dirname,
-        "./node_modules/bootstrap/dist/css/bootstrap.css"
-      ),
-      bootstrap_js: path.resolve(
-        __dirname,
-        "./node_modules/bootstrap/dist/js/bootstrap.js"
-      ),
-      popper_js: path.resolve(
-        __dirname,
-        "./node_modules/popper.js/dist/popper.js"
-      )
+    entry: {
+        main: "./src/js/index"
     },
-    extensions: [".js", ".jsx", ".json", ".css", ".scss"]
-  },
 
-  plugins: [extractCSS]
+    output: {
+        path: output_path,
+        //publicPath: "dist",
+        filename: "js/[name].js"
+    },
+
+    devtool: isDev ? "cheap-inline-module-source-map" : "source-map",
+
+    // TODO: delete devserver after BrowserSync will be fully tested
+    devServer: {
+        contentBase: "./dist",
+        watchContentBase: true
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules|dist|build)/,
+                use: {
+                    loader: "babel-loader",
+                    query: {
+                        presets: ["env"]
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: extractCSS.extract({
+                    fallback: "style-loader",
+                    use: "css-loader"
+                })
+            }
+        ]
+    },
+
+    resolve: {
+        alias: {
+            jquery: path.resolve(
+                __dirname,
+                "./node_modules/jquery/dist/jquery.js"
+            ),
+            bootstrap_css: path.resolve(
+                __dirname,
+                "./node_modules/bootstrap/dist/css/bootstrap.css"
+            ),
+            bootstrap_js: path.resolve(
+                __dirname,
+                "./node_modules/bootstrap/dist/js/bootstrap.js"
+            ),
+            popper_js: path.resolve(
+                __dirname,
+                "./node_modules/popper.js/dist/popper.js"
+            )
+        },
+        extensions: [".js", ".jsx", ".json", ".css", ".scss"]
+    },
+
+    plugins: [new CleanWebpackPlugin(pathsToClean, cleanOptions), extractCSS]
 };
